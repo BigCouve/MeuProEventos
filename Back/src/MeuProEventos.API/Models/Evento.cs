3c9bfb79001cba0376f8f32e3cc3c208abacc721
Douglas Augusto <douglassan1@hotmail.com>
2022-05-09 14:59:35 -0300

    Iniciado criação do model

---
+++ Back/src/MeuProEventos.API/Models/Evento.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuProEventos.API.Models
+{
+    public class Evento
+    {
+        
+    }
+}
