87939c6c814168bb4cae041052997186041daa6d
Douglas A <douglassan1@hotmail.com>
2022-05-19 22:58:52 -0300

    Em andamento: tabela do eventos.component populada com 3 linha de dados fictícios.

--- Front/MeuProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/MeuProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any;
+  public dados: any = [
+    {
+      id : 1,
+      Image : 'foto1.png',
+      Tema : 'Tema 1',
+      Local : 'Bahia',
+      Data : '19/05/2022',
+      QtdPessoas : 50,
+      Lote : 7,
+      Options : 'Deletar',
+    },
+    {
+      id : 2,
+      Image : 'foto2.png',
+      Tema : 'Tema 2',
+      Local : 'Santa Cartarina',
+      Data : '19/05/2022',
+      QtdPessoas : 250,
+      Lote : 15,
+      Options : 'Atualizar',
+    },
+    {
+      id : 3,
+      Image : 'foto3.png',
+      Tema : 'Tema 3',
+      Local : 'Acre',
+      Data : '19/05/2022',
+      QtdPessoas : 150,
+      Lote : 3,
+      Options : 'Adicionar',
+    }
+  ];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
