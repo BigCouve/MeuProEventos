24963c1740bec13d5305b0883a3af8576aa3e4db
Douglas Augusto <douglassan1@hotmail.com>
2022-05-20 14:54:38 -0300

    Concluído: visto sobre a diretiva ngIf Em andamento: começado a ver sobre a propriedade Binding.

--- Front/MeuProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/MeuProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,39 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
-  public dados: any = [
-    {
-      id : 1,
-      Image : 'foto1.png',
-      Tema : 'Tema 1',
-      Local : 'Bahia',
-      Data : '19/05/2022',
-      QtdPessoas : 50,
-      Lote : 7,
-      Options : 'Deletar',
-    },
-    {
-      id : 2,
-      Image : 'foto2.png',
-      Tema : 'Tema 2',
-      Local : 'Santa Cartarina',
-      Data : '19/05/2022',
-      QtdPessoas : 250,
-      Lote : 15,
-      Options : 'Atualizar',
-    },
-    {
-      id : 3,
-      Image : 'foto3.png',
-      Tema : 'Tema 3',
-      Local : 'Acre',
-      Data : '19/05/2022',
-      QtdPessoas : 150,
-      Lote : 3,
-      Options : 'Adicionar',
-    }
-  ];
+  public eventos: any = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -51,9 +19,6 @@ export class EventosComponent implements OnInit {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
       response =>this.eventos = response,
       error => console.log(error),
-
     );
-
   }
-
 }
